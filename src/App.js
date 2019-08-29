006b5d42148c15341bea80259356984f4f234ed2
qiuxan <qiuxan@gmail.com>
2019-08-29 10:37:45 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import Booklist from "./Booklist";
+const App = () => (
+  <section>
+    <h1>this is our application</h1>
+    <Booklist />
+  </section>
+);
 
 export default App;
---
+++ src/Booklist.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Book from "./Book";
+
+export default class Booklist extends Component {
+  state = {
+    books: [
+      {
+        book: "book1",
+        author: "author1"
+      },
+      {
+        book: "book2",
+        author: "author2"
+      },
+
+      {
+        book: "book3",
+        author: "author3"
+      }
+    ]
+  };
+
+  render() {
+    // const books = this.state.books.map(item => item.book);
+
+    // console.log(books);
+
+    return (
+      <section>
+        <h3>This is Booklist</h3>
+
+        {this.state.books.map((item, index) => (
+          <Book info={item} />
+        ))}
+      </section>
+    );
+  }
+}
